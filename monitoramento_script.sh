f05ad9c600972e5aa29ec6939c273b12e79e62c1
Carlos Eduardo <104033725+CarlosEdu20@users.noreply.github.com>
2025-07-02 13:06:04 -0300

    Update monitoramento_script.sh

--- monitoramento_script.sh
+++ monitoramento_script.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+!/bin/bash
 HTTP_STATUS=$(curl -s -w "%{http_code}" -o /dev/null http://localhost:80)
+DATA=$(date '+%Y-%m-%d %H:%M:%S')
+LOG_PATH="/var/log/monitoramento.log."
+URL_DISCORD=$(cat /projeto_linux/URL_Discord)
+
 if [ "$HTTP_STATUS" == "200" ];
 then
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] O servidor NGINX está funcionando (HTTP $HTTP_STATUS) " >>  /var/log/monitoramento.log
+        echo "[$DATA] O servidor Nginx está funcionando (HTTP $HTTP_STATUS) " >> $LOG_PATH
         echo "Serviço Online"
 else
-
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] O servidor NGINX está fora do ar (HTTP $HTTP_STATUS)  " >> /var/log/monitoramento.log
+        echo "[$DATA] O servidor Nginx está fora do ar (HTTP $HTTP_STATUS)  " >> $LOG_PATH
         echo "Serviço indisponível"
-        Url_Discord=$(cat /projeto_linux/URL_Discord)
-        curl -s -o /dev/null -X POST "$Url_Discord"\
-         -H "Content-Type: application/json"\
-         -d '{"content": "O servidor NGINX está fora do ar!"}'
+        if [ -n "$URL_DISCORD" ]; then
+                curl -s -o /dev/null -X POST "$URL_DISCORD"\
+                        -H "Content-Type: application/json"\
+                        -d '{"content": "O servidor Nginx está fora do ar!"}'
+        else
+                echo "Webhook do Discord não encontrado. Alerta não enviado." >> $LOG_PATH
+        fi
 
 
 fi
